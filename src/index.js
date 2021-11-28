0c427909aa0ee99623f0048466741485dd3ca300
Samu Garron <garron.garron@gmail.com>
2021-11-28 11:32:17 -0300

    Test 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ function handleFunc(evnet){
     var alpha = event.alpha;
     var beta = event.beta;
     var gamma = event.gamma;
-    document.body.innerHTML = JSON.stringify([alpha, beta, gamma])
-    
-    
-    // JSON.stringify(event)
+    document.body.querySelector('.deviceorientation').innerHTML = JSON.stringify([alpha, beta, gamma])
+    console.log(event);
 }
 
 window.addEventListener('deviceorientation', handleFunc, false);
+
+function handleMotion(e){
+    console.log(e);
+    document.body.querySelector('.devicemotion').innerHTML = JSON.stringify(e)+`
+    
+    ${e.acceleration.x}
+    ${e.acceleration.y}
+    ${e.acceleration.z}
+    ${e.rotationRate.alpha}
+    ${e.rotationRate.beta}
+    ${e.rotationRate.gamma}
+    
+    
+    `;
+    
+}
+window.addEventListener("devicemotion", handleMotion)
