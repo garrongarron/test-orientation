df4eaa770e2e2a1cd59558ea3ada63fb983b2267
Samu Garron <garron.garron@gmail.com>
2021-11-30 07:26:36 -0300

    Test 7 v0.0.4

--- src/a.js
+++ src/a.js
@@ -1,9 +1,9 @@
 const circle = document.querySelector('.circle')
 function handleMotion(e) {
     let deg = (e.accelerationIncludingGravity.y/9.8)*90
-    deg = Math.round(deg)
+    deg = Math.round(45)
     circle.innerHTML = deg
-    circle.style.transform  = `rotate(${deg}deg) !important`
+    circle.style.transform  = `rotate(${deg}deg)`
     document.body.querySelector('.devicemotion').innerHTML = `
     accelerationIncludingGravity <br />
     x : ${e.accelerationIncludingGravity.x}<br />
